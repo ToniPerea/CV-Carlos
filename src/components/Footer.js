89f8eee718d727581ce0cf4e446342b1ade0d811
ToniPerea <antonioperea31@gmail.com>
2021-09-13 16:53:52 +0200

    comienzo

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const FooterComponent = (props) =>{
+    return(
+        <>
+        <h1>soy el FooterComponent</h1>
+        </>
+    );
+}
+
+export default FooterComponent
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+const NavBarComponent = (props) =>{
+    return(
+        <>
+        <h1>soy el NavBarComponent</h1>
+        </>
+    );
+}
+
+export default NavBarComponent
