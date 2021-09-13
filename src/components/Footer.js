96e7f755480d5fc11bca7101dd3913cf72621ca8
ToniPerea <antonioperea31@gmail.com>
2021-09-13 18:57:23 +0200

    improve icons footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,24 @@
 import { faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Card,Button, Row, Container } from "react-bootstrap";
+import { Card,Button, Row, Col,Container } from "react-bootstrap";
 
 
 const FooterComponent = (props) =>{
     return (
       <>
         <Container className="text-center">
-            <Row>
+          <Row>
             <p>Follow me on Instagram and Twitter to get the latest updates.</p>
-            </Row>
-            <Row>
-                <FontAwesomeIcon className="d-block m-auto w-25" icon={faTwitter}/>
-                <FontAwesomeIcon className="d-block m-auto w-25" icon={faInstagram}/>
-            </Row>
+          </Row>
+          <Row className="justify-content-center fixed">
+            <Col  className="fixed d-block px-4"  xs={1} style={{/*marginRight:'-50px, backgroundColor:'blue*/width:'0.875em'}} >
+              <FontAwesomeIcon size="2x" icon={faTwitter} />
+              
+            </Col>
+            <Col  className="fixed px-4"  style={{width:'0.875em'}} xs={1}>
+            <FontAwesomeIcon size="2x" icon={faInstagram} />
+            </Col>
+          </Row>
         </Container>
       </>
     );
--- src/components/Fotos.js
+++ src/components/Fotos.js
@@ -3,7 +3,7 @@ import { Container, Image, Navbar, Row } from "react-bootstrap";
 const Fotos = (props) => {
     return (
         <>
-            <Container style={{backgroundColor:'blue', maxWidth:'100%', padding:'0'}}>
+            <Container style={{ maxWidth:'100%', padding:'0'}}>
                 <Image style={{width:'100%',objectFit:'contain', maxWidth:'100%'}}src="img/Handmade.jpg"/>
             </Container>
 
