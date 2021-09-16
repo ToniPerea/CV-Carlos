9eef2c9e0501a9294cdf304d109d87d087e00e7b
ToniPerea <antonioperea31@gmail.com>
2021-09-16 18:00:55 +0200

    mejora del navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,18 +4,21 @@ import { Container, Navbar, Nav, Image } from "react-bootstrap";
 const NavBarComponent = (props) =>{
     return (
       <>
-        <Navbar variant="dark">
+        <Navbar expand="lg">
           <Container>
             <Navbar.Brand href="/">
-              <Image style={{width:'110px'}}src="img/Logo-Revalotti.png" fluid />
+              <Image
+                style={{ width: "110px" }}
+                src="img/Logo-Revalotti.png"
+                fluid
+              />
             </Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar className="justify-content-end">
+            <Nav className=" mx-3 justify-content-end">
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/portfolio">Portfolio</Nav.Link>
               <Nav.Link href="#pricing">About</Nav.Link>
               <Nav.Link href="#pricing">Contact</Nav.Link>
-            </Navbar>
+            </Nav>
           </Container>
         </Navbar>
       </>
