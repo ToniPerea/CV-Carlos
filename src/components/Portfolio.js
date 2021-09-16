28b826f2442c61dbb2f725896e6578d923b1c014
ToniPerea <antonioperea31@gmail.com>
2021-09-16 12:36:56 +0200

    Arreglo columnas pagina Portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,76 @@
+import { Row, Col, Container} from "react-bootstrap";
 
 
 const Portfolio = (props) =>{
-    return(
-        <>
-        
-        </>
+    return (
+      <>
+        <Container className="container-fluid mt-5 mb-5">
+          <Row className="mt-4 justify-content-center justify-content-center">
+            <Col xs={12} md={5} className="mx-3 mb-4" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+            <Col xs={12} md={5} className="mx-3" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+          </Row>
+          <Row className="mt-4 justify-content-center">
+            <Col xs={12} md={5} className="mx-3 mb-4" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+            <Col xs={12} md={5} className="mx-3" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+          </Row>
+          <Row className="mt-4 justify-content-center">
+            <Col xs={12} md={5} className="mx-3 mb-4" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+            <Col xs={12} md={5} className="mx-3" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+          </Row>
+          <Row className="mt-4 justify-content-center">
+            <Col xs={12} md={5} className="mx-3 mb-4" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+            <Col xs={12} md={5} className="mx-3" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+          </Row>
+          <Row className="mt-4 justify-content-center">
+            <Col xs={12} md={5} className="mx-3 mb-4" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+            <Col xs={12} md={5} className="mx-3" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+          </Row>
+          <Row className="mt-4 justify-content-center">
+            <Col xs={12} md={5} className="mx-3 mb-4" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+            <Col xs={12} md={5} className="mx-3" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+          </Row>
+          <Row className="mt-4 justify-content-center">
+            <Col xs={12} md={5} className="mx-3 mb-4" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+            <Col xs={12} md={5} className="mx-3" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+          </Row>
+          <Row className="mt-4 justify-content-center">
+            <Col xs={12} md={5} className="mx-3 mb-4" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+            <Col xs={12} md={5} className="mx-3" style={{ backgroundColor: "purple", height:'230px' }}>
+              <h1>antonio</h1>
+            </Col>
+          </Row>
+        </Container>
+      </>
     );
 }
 
