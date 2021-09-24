9382259e429c681f54bf36dd1ff776e9cf7630fd
ToniPerea <antonioperea31@gmail.com>
2021-09-24 12:43:38 +0200

    navbar toggle improvement

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,20 +4,26 @@ import { Container, Navbar, Nav, Image } from "react-bootstrap";
 const NavBarComponent = (props) =>{
     return (
       <>
-        <Navbar>
+        <Navbar expand="lg">
           <Container>
             <Navbar.Brand href="/">
-              <Image
-                style={{ width: "110px" }}
-                src="img/Logo-Revalotti.png"
-                fluid
-              />
+              <Image style={{ width: "110px" }} src="img/Logo-Revalotti.png" />
             </Navbar.Brand>
+            <Navbar.Toggle />
             <Navbar.Collapse className="justify-content-end">
               <Nav className=" mx-3 ">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
+                <Nav.Link className="mx-auto" href="/">
+                  Home
+                </Nav.Link>
+                <Nav.Link className="mx-auto" href="/portfolio">
+                  Portfolio
+                </Nav.Link>
+                <Nav.Link className="mx-auto" href="/about">
+                  About
+                </Nav.Link>
+                <Nav.Link className="mx-auto" href="#pricing">
+                  Contact
+                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
